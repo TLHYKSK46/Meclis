fe4371d9106ed4d82f884f5f06a75d0b2a4ee3e9
oem <oem@DESKTOP-LFR4BPK>
2021-03-01 23:18:14 +0300

    Vekil Detay Hariç 01.03.2021 Tüm Tablolar

---
+++ MeclisEntities/Entities/DilTanim.cs
@@ -0,0 +1,19 @@
+﻿using MeclisEntities.IEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeclisEntities.Entities
+{
+    public class DilTanim:IEntity
+     
+    {
+        public int Id { get; set; }
+        public string DilAdi { get; set; }
+        public string DilSeviye { get; set; }
+       
+
+    }
+} 
---
+++ MeclisEntities/Entities/IlTanim.cs
@@ -0,0 +1,20 @@
+﻿using MeclisEntities.IEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeclisEntities.Entities
+{
+    public class IlTanim:IEntity
+     
+    {
+        public int Id { get; set; }
+        public int IlKodu { get; set; }
+        public string IlAdi { get; set; }
+        public string IlBolgesi { get; set; }
+
+
+    }
+} 
