4b0f1079460cb39178a0fe7c0dd93fc8c2082e90
Talha YÜKSEK <yuksek_talha@hotmail.com>
2021-03-10 09:14:05 +0300

    Tüm Tabloların DbSet leri Eklendi...

--- MeclisDal/Dal/MeclisContext.cs
+++ MeclisDal/Dal/MeclisContext.cs
@@ -14,6 +14,17 @@ namespace MeclisDal.Dal
         public DbSet<VekilTanim> VekilTanims { get; set; }
         public DbSet<CinsiyetTanim> CinsiyetTanims { get; set; }
         public DbSet<DilTanim> DilTanims { get; set; }
+        public DbSet<DonemTanim> DonemTanims { get; set; }
+        public DbSet<DoslukGrupTanim> DoslukGrupTanims { get; set; }
+        public DbSet<GenelMerkezGorev> GenelMerkezGorevs { get; set; }
+        public DbSet<GrupPersonelTanim> GrupPersonelTanims { get; set; }
+        public DbSet<HatirlatmaTanim> HatirlatmaTanims { get; set; }
+        public DbSet<KomisyonTanim> KomisyonTanims { get; set; }
+        public DbSet<MazeretKod> MazeretKods { get; set; }
+        public DbSet<MazeretTanim> MazeretTanims { get; set; }
+        public DbSet<MeclisGorevTanim> MeclisGorevTanims { get; set; }
+        public DbSet<PartiTanim> PartiTanims { get; set; }
+        public DbSet<VekilDanisman> VekilDanismans { get; set; }
         // public DbSet<Category> Categories { get; set; }
 
     }
