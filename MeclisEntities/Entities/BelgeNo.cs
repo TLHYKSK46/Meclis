f961748e7d47d98bb40f462dabefa218f018793a
TLHYKSK46 <yuksek_talha@hotmail.com>
2021-06-12 17:00:50 +0300

    ss

---
+++ MeclisEntities/Entities/BelgeNo.cs
@@ -0,0 +1,21 @@
+﻿using MeclisEntities.IEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeclisEntities.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeclisEntities.Entities
+{
+    public class BelgeNo:EntBase
+     
+    {
+   
+       
+        public string BelgeNo1 { get; set; }
+
+
+    }
+} 
