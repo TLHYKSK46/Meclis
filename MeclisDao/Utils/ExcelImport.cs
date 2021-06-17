ca92600114878c9cd0e315a5e052fcaefdf77ab6
TLHYKSK46 <yuksek_talha@hotmail.com>
2021-06-18 00:04:08 +0300

    excel aktarım güncelleme yapıldı.

--- MeclisDao/Utils/ExcelImport.cs
+++ MeclisDao/Utils/ExcelImport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,21 @@ namespace MeclisDao.Utils
    public class ExcelImport
     {
         public static DataGridView ExcelYukle(string dosyaYolu) {
-
-            //versiyona göre değişiklik oluyor..
-           // OleDbConnection theConnection = new OleDbConnection(@"provider=Microsoft.Jet.OLEDB.4.0;data source='" + dosyaYolu + "';Extended Properties=\"Excel 8.0;HDR=NO;IMEX=1;\"");
-            OleDbConnection theConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dosyaYolu + ";Extended Properties='Excel 12.0 XML;HDR=YES;';");
+            OleDbConnection theConnection;
+            var extension= Path.GetExtension(dosyaYolu);
+            switch (extension)
+            {
+                case "xls":
+                    theConnection = new OleDbConnection(@"provider=Microsoft.Jet.OLEDB.4.0;data source='" + dosyaYolu + "';Extended Properties=\"Excel 8.0;HDR=NO;IMEX=1;\"");
+                    break;
+                case "xlsx":
+                    theConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dosyaYolu + ";Extended Properties='Excel 12.0 XML;HDR=YES;';");
+                    break;
+                default:
+                    throw new Exception("Desteklenmeyen Format");
+            }
+            //versiyona göre değişiklik oluyor..            
+           // OleDbConnection 
             theConnection.Open();
             OleDbDataAdapter thDataAdapter = new OleDbDataAdapter("Select * from [Sayfa1$]", theConnection);
             DataSet thSd = new DataSet();
