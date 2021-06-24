00cbc9c0244266ac3290dcfe5bd55f521b5d0483
oem <oem@DESKTOP-LFR4BPK>
2021-06-25 00:19:24 +0300

    11

---
+++ MeclisEntities/Entities/VekilYoklama.cs
@@ -0,0 +1,20 @@
+﻿using MeclisEntities.IEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeclisEntities.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace MeclisEntities.Entities
+{
+    public class VekilYoklama:EntBase
+     
+    {
+        public int VekilTanimId { get; set; }
+        public int OturumId { get; set; }
+        public bool Katildimi { get; set; }
+        public bool Pusulali { get; set; }
+    }
+} 
---
+++ MeclisEntities/Mappings/VekilYoklamaMap.cs
@@ -0,0 +1,30 @@
+﻿using MeclisEntities.Entities;
+using MeclisEntities.Mappings.BaseMap;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeclisEntities.Mappings
+{
+    public class VekilYoklamaMap : BaseMap<VekilYoklama>
+    {
+        public VekilYoklamaMap() {
+            ToTable(@"vekil_yoklama", @"dbo");
+            //HasKey(x => x.Id);
+          //  Property(x => x.Id).HasColumnName("Id");
+            Property(x => x.OturumId).HasColumnName("oturum_id");
+            Property(x => x.VekilTanimId).HasColumnName("vekil_tanim_id");
+            Property(x => x.Pusulali).HasColumnName("pusulali");
+            Property(x => x.Katildimi).HasColumnName("katildimi");
+            Property(x => x.Pusulali).HasColumnName("pusulali");
+
+
+
+
+
+        }
+    }
+}
